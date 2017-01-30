9d5bcdd614714378565badaa3587e3e11370f213
EternalPhane <eternalphane@gmail.com>
2017-01-30 22:19:04 +0800

    fix bugs

--- pip-upgrade.py
+++ pip-upgrade.py
@@ -1,6 +1,5 @@
-# encoding: utf-8
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 """Upgrade all out-dated packages using pip."""
 
 import os
@@ -15,7 +14,7 @@ import pip
 __author__ = 'EternalPhane'
 __copyright__ = 'Copyright (c) 2016 EternalPhane'
 __license__ = 'MIT'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __maintainer__ = 'EternalPhane'
 __email__ = 'eternalphane@gmail.com'
 __status__ = 'Prototype'
@@ -27,22 +26,18 @@ def main():
         bar = int(per * 0.2 + 0.5)
         print('\r[%s%s] %.2f%%' % ('=' * bar, ' ' * (20 - bar), 100 if per > 100 else per), end='')
 
-    system = {
-        'Windows': 'win',
-        'Linux': 'manylinux1',
-        'Darwin': 'macos',
-        '': ''
-    }[platform.system()]
+    system = {'Windows': 'win', 'Linux': 'manylinux1', 'Darwin': 'macos', '': ''}[platform.system()]
     arch = {
         '32bit': '32' if system == 'win' else 'i686' if system == 'manylinux1' else '',
         '64bit': 'amd64' if system == 'win' else 'x86_64' if system else '',
         '': ''
     }[platform.architecture()[0]]
     impl = platform.python_implementation()
-    py_ver = 'cp' + ''.join(platform.python_version_tuple()[:2]) if impl == 'CPython' else 'source'
+    py_ver = platform.python_version_tuple()[:2]
+    py_vers = ('cp%s%s' % py_ver, '%s.%s' % py_ver, 'py' + py_ver[0]) if impl == 'CPython' else ()
     pkgs = []
     pypi = xmlrpc.client.ServerProxy('https://pypi.python.org/pypi')
-    dists = pip.get_installed_distributions()
+    dists = sorted(pip.get_installed_distributions(), key=lambda pkg: pkg.key)
     max_len = len(dists[0].project_name)
     for dist in dists:
         dst_len = len(dist.project_name)
@@ -50,34 +45,32 @@ def main():
         max_len = dst_len if dst_len > max_len else max_len
         found = False
         for version in sorted(
-            (version for version in try_until_success(
-                lambda: pypi.package_releases(dist.project_name, True),
-                lambda: print('\n')
-            ) if V.parse(version) > V.parse(dist.version)),
-            key=lambda version: V.parse(version),
-            reverse=True
+            (
+                version
+                for version in try_until_success(
+                    lambda: pypi.package_releases(dist.project_name, True), lambda: print('\n')
+                ) if V.parse(version) > V.parse(dist.version)
+            ),
+                key=lambda ver: V.parse(ver),
+                reverse=True
         ):
             for release in try_until_success(
-                lambda: pypi.release_urls(dist.project_name, version),
-                lambda: print('\n')
+                    lambda: pypi.release_urls(dist.project_name, version), lambda: print('\n')
             ):
-                if ((
-                        release['python_version'] == py_ver or
-                        py_ver in release['filename'] or
-                        "py2.py3" in release['filename']
-                    ) and ((
-                            system in release['filename'] and
-                            arch in release['filename']
-                        ) or
-                        "none-any" in release['filename']
+                if ((release['python_version'] in py_vers or any(ver in release['filename'] for ver in py_vers)) and
+                    ((system in release['filename'] and arch in release['filename']) or
+                     "none-any" in release['filename']) or release['python_version'] == 'source'):
+                    pkgs.append(
+                        type(
+                            '', (object, ), {
+                                'name': dist.project_name,
+                                'version': (dist.version, version),
+                                'filename': release['filename'],
+                                'url': release['url'],
+                                'type': 'sdist' if release['python_version'] == 'source' else 'wheel'
+                            }
+                        )
                     )
-                ):
-                    pkgs.append(type('', (object,), {
-                        'name': dist.project_name,
-                        'version': (dist.version, version),
-                        'filename': release['filename'],
-                        'url': release['url']
-                    }))
                     found = True
                     break
             if found:
@@ -86,7 +79,7 @@ def main():
     if len(pkgs):
         print('The following packages will be upgraded:')
         for pkg in pkgs:
-            print(' %s (Current: %s Lastest: %s)' % (pkg.name, pkg.version[0], pkg.version[1]))
+            print(' %s (Current: %s Lastest: %s) [%s]' % (pkg.name, pkg.version[0], pkg.version[1], pkg.type))
         if input('Do you want to continue? [Y/n] ') not in 'yY':
             print('Abort.')
         else:
@@ -96,9 +89,7 @@ def main():
                 print('Downloading %s...' % (pkg.name))
                 pkg_cache = try_until_success(
                     lambda: urllib.request.urlretrieve(
-                        pkg.url,
-                        filename=os.path.join(tempfile.tempdir, pkg.filename),
-                        reporthook=schedule
+                        pkg.url, filename=os.path.join(tempfile.tempdir, pkg.filename), reporthook=schedule
                     ),
                     lambda: None if input('\nDo you want to retry? [Y/n] ') in 'yY' else 0
                 )[0]
